b1f2b85a5c6036fe7e0ba2c45d4c791063c0d1b8
Andrey Zhuravel <milan23.28.01@gmail.com>
2024-05-29 20:45:41 +0300

    add joinclass helper

---
+++ src/helpers/JoinClass.ts
@@ -0,0 +1,9 @@
+export class JoinClass {
+  static merge(...classNames: Array<string | undefined>): string {
+    return classNames.filter((cn) => cn !== undefined).join(" ");
+  }
+
+  static addPrefix(prefix: string, className: string): string {
+    return prefix.concat(className);
+  }
+}
