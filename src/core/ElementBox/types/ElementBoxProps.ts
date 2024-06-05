fd39ba2de7d5a5f9784052f27229ce294378e05c
Andrey Zhuravel <andrey.zhuravel.01.23.94@gmail.com>
2024-06-05 23:25:04 +0300

    add layout props

--- src/core/ElementBox/types/ElementBoxProps.ts
+++ src/core/ElementBox/types/ElementBoxProps.ts
@@ -1,6 +1,6 @@
 type CssProps = React.CSSProperties;
 
-interface ElementBoxStyleProps {
+interface Typography {
   fz?: CssProps["fontSize"];
   fw?: Fw | CssProps["fontWeight"];
   lh?: CssProps["lineHeight"];
@@ -14,6 +14,21 @@ interface ElementBoxStyleProps {
   text_align?: CssProps["textAlign"];
   vertical_align?: CssProps["verticalAlign"];
 }
+interface Layout {
+  position?: CssProps["position"];
+  top?: CssProps["top"];
+  right?: CssProps["right"];
+  bottom?: CssProps["bottom"];
+  left?: CssProps["left"];
+  visibility?: CssProps["visibility"];
+  overflow?: CssProps["overflow"];
+  overflow_x?: CssProps["overflowX"];
+  overflow_y?: CssProps["overflowY"];
+  zIndex?: CssProps["zIndex"];
+  display?: CssProps["display"];
+}
+
+interface ElementBoxStyleProps extends Typography, Layout {}
 
 interface ElementBoxReactProps
   extends Omit<React.HTMLProps<HTMLElement>, "color"> {}
