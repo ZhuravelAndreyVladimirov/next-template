0c456d1287ecc97f17797fd532ca3d3f5d3ad469
Andrey Zhuravel <andrey.zhuravel.01.23.94@gmail.com>
2024-06-06 12:06:08 +0300

    add flex & grid props

--- src/core/ElementBox/types/ElementBoxProps.ts
+++ src/core/ElementBox/types/ElementBoxProps.ts
@@ -1,3 +1,4 @@
+type Fw = "100" | "200" | "300" | "400" | "500" | "600" | "700" | "800" | "900";
 type CssProps = React.CSSProperties;
 
 interface Typography {
@@ -28,7 +29,26 @@ interface Layout {
   display?: CssProps["display"];
 }
 
-interface ElementBoxStyleProps extends Typography, Layout {}
+interface GridFlex {
+  flex?: CssProps["flex"];
+  flex_basis?: CssProps["flexBasis"];
+  flex_direction?: CssProps["flexDirection"];
+  flex_wrap?: CssProps["flexWrap"];
+  flex_grow?: CssProps["flexGrow"];
+  flex_shrink?: CssProps["flexShrink"];
+  order?: CssProps["order"];
+  g_cols?: CssProps["gridTemplateColumns"];
+  g_rows?: CssProps["gridTemplateRows"];
+  gaf?: CssProps["gridAutoFlow"];
+  gac?: CssProps["gridAutoColumns"];
+  gar?: CssProps["gridAutoRows"];
+  gap?: CssProps["gap"];
+  justify?: CssProps["justifyContent"];
+  align?: CssProps["alignItems"];
+  place?: CssProps["placeContent"];
+}
+
+interface ElementBoxStyleProps extends Typography, Layout, GridFlex {}
 
 interface ElementBoxReactProps
   extends Omit<React.HTMLProps<HTMLElement>, "color"> {}
@@ -39,5 +59,3 @@ export interface ElementBoxProps
   scale?: number;
   tag?: string;
 }
-
-type Fw = "100" | "200" | "300" | "400" | "500" | "600" | "700" | "800" | "900";
