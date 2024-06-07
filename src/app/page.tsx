7c8f030722e4f934fbdd1bd65dbe44234dbd62b9
Andrey Zhuravel <milan23.28.01@gmail.com>
2024-06-07 21:35:32 +0300

    clear page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,3 @@
-import { UiText } from "@/ui/UiText/UiText";
-
 export default function Home() {
-  return (
-    <>
-      <UiText
-        fz={30}
-        fw="500"
-        lh={123}
-        ff="--font-roboto-regular"
-        color="red"
-        fs="italic"
-        font="revert"
-        fv="all-small-caps"
-        fv_num="oldstyle-nums"
-        text_align="center"
-        vertical_align={"text-bottom"}
-      >
-        Hello world! 1234567
-      </UiText>
-    </>
-  );
+  return;
 }
