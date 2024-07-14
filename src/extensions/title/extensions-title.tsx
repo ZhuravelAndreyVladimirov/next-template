6649416920c5eb7da389ab6eea611a4a185855d5
Andrey Zhuravel <andrey.zhuravel.01.23.94@gmail.com>
2024-07-14 15:52:08 +0300

    refactore

--- src/extensions/title/extensions-title.tsx
+++ src/extensions/title/extensions-title.tsx
@@ -3,9 +3,6 @@ import { Title } from "@mantine/core";
 import styles from "./extensions-title.module.scss";
 
 export const ExtensionTitle = Title.extend({
-  defaultProps: {
-    c: "var(--title-c)",
-  },
   classNames: () => {
     return {
       root: styles.root,
