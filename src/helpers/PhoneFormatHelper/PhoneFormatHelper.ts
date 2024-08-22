658b46509d10490cdcc9ae09b5593409a041daa0
Andrey Zhuravel <andrey.zhuravel.01.23.94@gmail.com>
2024-08-22 19:44:20 +0300

    nt-1 create seriakization method to phone format helper

--- src/helpers/PhoneFormatHelper/PhoneFormatHelper.ts
+++ src/helpers/PhoneFormatHelper/PhoneFormatHelper.ts
@@ -1,4 +1,5 @@
 type PhoneMask = null | number | string | undefined;
+type PhoneSerialization = null | number | string | undefined;
 
 export class PhoneFormatHelper {
   static mask(phone: PhoneMask): string {
@@ -6,9 +7,23 @@ export class PhoneFormatHelper {
       return "";
     }
 
-    return String(phone).replace(
-      /(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})/,
-      "+$1 ($2) $3-$4-$5",
+    const val = String(PhoneFormatHelper.serialization(phone));
+
+    return (
+      `+${val.substring(0, 1)} (${val.substring(1, 4)}) ` +
+      `${val.substring(4, 7)}-${val.substring(7, 9)}-${val.substring(9, 11)}`
     );
+  }
+
+  static serialization(phone: PhoneSerialization): null | number {
+    if (!phone && typeof phone !== "string") {
+      return null;
+    }
+
+    if (typeof phone === "number") {
+      return phone;
+    }
+
+    return Number.parseInt(phone.replace(/[^0-9]/g, ""));
   }
 }
