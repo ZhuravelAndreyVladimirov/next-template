4bcd86f9f48f8b4451fc93e63098e8df7daec411
Andrey Zhuravel <andrey.zhuravel.01.23.94@gmail.com>
2024-08-22 21:32:56 +0300

    and-2 create serializationint method to price format helper

--- src/helpers/PriceFormatHelper/PriceFormatHelper.ts
+++ src/helpers/PriceFormatHelper/PriceFormatHelper.ts
@@ -1,1 +1,19 @@
-export class PriceFormatHelper {}
+type SerializationPrice = null | number | string | undefined;
+
+export class PriceFormatHelper {
+  static serializationInt(price: SerializationPrice): null | number {
+    if (!price && typeof price !== "string" && typeof price !== "number") {
+      return null;
+    }
+
+    if (typeof price === "number") {
+      if (Number.isInteger(price)) {
+        return price;
+      }
+
+      return Number.parseInt(String(price));
+    }
+
+    return Number.parseInt(price.replace(/[^0-9]/g, ""));
+  }
+}
--- src/helpers/PriceFormatHelper/test/PriceHelper.spec.ts
+++ src/helpers/PriceFormatHelper/test/PriceHelper.spec.ts
@@ -1,1 +1,43 @@
-describe("Сериализация PriceFormatHelper", () => {});
+import { PriceFormatHelper as PH } from "../PriceFormatHelper";
+
+describe("Сериализация в int PriceFormatHelper метод serializationInt", () => {
+  it("Проверка если передать null в метод serializationInt должно вернуть null", () => {
+    const price = PH.serializationInt(null);
+
+    expect(price).toBe(null);
+  });
+
+  it("Проверка если передать undefined в метод serializationInt должно вернуть null", () => {
+    const price = PH.serializationInt(undefined);
+
+    expect(price).toBe(null);
+  });
+
+  it("Проверка если передать число 1234 в метод serializationInt должно вернуть 1234", () => {
+    const price = PH.serializationInt(1234);
+
+    expect(price).toBe(1234);
+  });
+  it("Проверка если передать число 1234.12 в метод serializationInt должно вернуть 1234", () => {
+    const price = PH.serializationInt(1234.12);
+
+    expect(price).toBe(1234);
+  });
+  it("Проверка если передать строку '1234' в метод serializationInt должно вернуть 1234", () => {
+    const price = PH.serializationInt("1234");
+
+    expect(price).toBe(1234);
+  });
+
+  it("Проверка если передать строку '1234.123' в метод serializationInt должно вернуть 1234123", () => {
+    const price = PH.serializationInt("1234.123");
+
+    expect(price).toBe(1234123);
+  });
+
+  it("Проверка если передать строку '12sad34.1asd23' в метод serializationInt должно вернуть 1234123", () => {
+    const price = PH.serializationInt("12asd34.1asd23");
+
+    expect(price).toBe(1234123);
+  });
+});
