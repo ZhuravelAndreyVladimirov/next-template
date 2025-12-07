054a2852d427f0a9be2bb9c6254d62db013cc524
Andrey <andrey.zhuravel.01.23.94@gmail.com>
2025-12-08 00:42:55 +0300

    fix husky

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,15 +1,9 @@
+echo "husky - DEPRECATED
+
+Please remove the following two lines from $0:
+
 #!/usr/bin/env sh
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
 
-if [ -z "$husky_skip_init" ]; then
-  husky_skip_init=1
-  export husky_skip_init
-
-  if [ -n "$HUSKY" ]; then
-    echo "husky - skipping hooks (HUSKY=$HUSKY)"
-    exit 0
-  fi
-
-  if [ -n "$CI" ]; then
-    echo "husky - running in CI, hooks are enabled"
-  fi
-fi
+They WILL FAIL in v10.0.0
+"
